6fe428fb20d8cbe89b1463ce12f6d3eb0f098f29
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-09-28 22:48:25 -0500

    Initial commit

---
+++ alumno_constructor_classes/Alumno.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace alumno_constructor_classes
+{
+    internal class Alumno
+    {
+        //Propiedades
+        private string Nombre;
+        private int Edad;
+        //Constructor
+        public Alumno()
+        {
+            Nombre = "Abelardo Cruz Leos";
+            Edad = 20;
+        }
+        //Metodos
+        public void Imprimir_Nombre_Alumno()
+        {
+            Console.WriteLine("El Nombre del Alumno es: " + Nombre);
+        }
+        public void Imprimir_Edad_Alumno()
+        {
+            Console.WriteLine("La edad del Alumno es: " + Edad);
+        }
+        public void Imprimir_si_es_mayor_o_menor_Edad_Alumno()
+        {
+            if (Edad >=18)
+            {
+                Console.WriteLine("El Alumno es mayor de edad tiene: " + Edad);
+            }
+            else
+            {
+                Console.WriteLine("El Alumno es menor de edad tiene: " + Edad);
+            }
+        }
+    }
+}
---
+++ alumno_constructor_classes/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using alumno_constructor_classes;
+
+Console.WriteLine("Hello, progrmama alumno");
+Alumno alumno1 = new Alumno();
+alumno1.Imprimir_Nombre_Alumno();
+alumno1.Imprimir_Edad_Alumno();
+alumno1.Imprimir_si_es_mayor_o_menor_Edad_Alumno();
